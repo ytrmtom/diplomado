a05bfc84d2b8b4a11be594aaff3a86190bb1ddcf
ytrmtom <ytrmtom@gmail.com>
2025-06-29 16:32:18 -0400

    modificaciones en config database y task

--- src/database/database.js
+++ src/database/database.js
@@ -15,7 +15,7 @@ console.log('Database config:', {
 
 export default sequelize; */
 
-export const sequelize = new Sequelize(
+const sequelize = new Sequelize(
     config.DB_DATABASE,
     config.DB_USER,
     config.DB_PASSWORD,
@@ -23,13 +23,14 @@ export const sequelize = new Sequelize(
         host: config.DB_HOST,
         dialect: config.DB_DIALECT,
         logging: console.log,
-        dialectOptions: 
-            config.DB_USE_SSL === "true" ? {
-                ssl: {
-                    require: true,
-                    rejectUnauthorized: false
-                }
-            } 
-            : {} 
-        }
+        dialectOptions: config.DB_USE_SSL === "true" ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        } : {}
+    }
 );
+
+export { sequelize };
+export default sequelize;
